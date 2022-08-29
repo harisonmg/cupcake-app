11d739068a93ee63c8158d2ccd97930d9a11c3a4
Harison Gachuru <harisongachuru@gmail.com>
2022-08-29 20:56:06 +0300

    Update pickup and summary fragment to use view model

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,18 +3,32 @@ package com.example.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrderViewModel : ViewModel() {
-    private val _quantity = MutableLiveData<Int>(0)
-    private val _flavour = MutableLiveData<String>("")
-    private val _date = MutableLiveData<String>("")
-    private val _price = MutableLiveData<Double>(0.0)
+    private val _quantity = MutableLiveData<Int>()
+    private val _flavour = MutableLiveData<String>()
+    private val _date = MutableLiveData<String>()
+    private val _price = MutableLiveData<Double>()
+    val dateOptions = getPickupOptions()
 
     // backing properties
     val quantity: LiveData<Int> = _quantity
     val flavour: LiveData<String> = _flavour
     val date: LiveData<String> = _date
     val price: LiveData<Double> = _price
+
+    init {
+        resetOrder()
+    }
+
+    private fun resetOrder() {
+        _quantity.value = 0
+        _flavour.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
 
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
@@ -26,6 +40,17 @@ class OrderViewModel : ViewModel() {
 
     fun setFlavour(desiredFlavour: String) {
         _flavour.value = desiredFlavour
+    }
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
     }
 
     fun setDate(pickupDate: String) {
